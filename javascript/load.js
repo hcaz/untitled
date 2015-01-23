98abd4846cd8a65af92ebf2b424ea6c79db6f05f
Zachary Claret-Scott <github@hcaz.me.uk>
2015-01-23 22:18:48 +0000

    More movements in place and posting to api
    Cleane dup moving and added outer bounds, started work on posting to the
    api, almost bought pizza

--- javascript/load.js
+++ javascript/load.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
 	$("#loader > p").html("Loading game-play scripts<br />Do not refresh page.");
 		$("head").append("<script type=\"text/javascript\" src=\"/javascript/player.js\" defer></script>");
 	$("#loader > p").html("Getting player details<br />Do not refresh page.");
-		window.player = player_set_name();
+		window.localplayer = player_set_name();
 	$("#loader > p").html("Contacting server<br />Do not refresh page.");
 	$("#loader").fadeOut("fast");
+	player_map();
 	player_sync();
 	window.setInterval(function(){
 		player_sync();
--- javascript/player.js
+++ javascript/player.js
@@ -1,49 +1,73 @@
-var players = [["test","000000",100,200]];
+var players = [["test","eee",5300,5000]];
 function player_display(){
-	players.push([window.player,"000000",0,0]);
 	for(var i=0;i<players.length;i++){
 		player = "player_"+players[i][0];
-		x = player_location_x(players[i][2]);
-		y = player_location_y(players[i][3]);
+		x = players[i][2];
+		y = players[i][3];
 		if($('#'+player).length){
 			x = $('#'+player).position().left - x;
 			y = $('#'+player).position().top - y;
 			$('#'+player).animate({left: "+="+x, top:"+="+y}, 10000);
 		}else{
-			$("#players").append("<div class=\"player\" id=\""+player+"\" style=\"top:"+y+"px;left:"+x+"px;\"></div>");
+			$("#players").append("<div class=\"player\" id=\""+player+"\" style=\"top:"+y+"px;left:"+x+"px;background: #"+players[i][4]+";\"></div>");
 		}
 	}
 }
+function player_map(){
+	$("#players").append("<div class=\"player\" id=\"player_"+window.localplayer+"\" style=\"top:5000px;left:5000px;\"></div>");
+	x = 5000 - ($(document).width() / 2);
+	y = 5000 - ($(document).height() / 2);
+	$('#players').css("left", "-"+x);
+	$('#players').css("top", "-"+y);
+}
 function player_sync(){
-	console.log("Sync");
+	$.post("/api", {name:window.localplayer,x:$('#player_'+window.localplayer).position().left,y:$('#player_'+window.localplayer).position().top})
+	.done(function(data){
+		console.log( "Data Loaded: " + data );
+	});
 	player_display();
 }
 
 $(document).keydown(function(e) {
-	speed = 2;
+	speed = 4;
 	switch(e.which) {
-		case 37: // left
-		case 65: // a
-		$('#players').animate({left: "-="+speed}, 10);
-		break;
-
-		case 38: // up
-		case 87: // w
-		$('#players').animate({top:"-="+speed}, 10);
-		break;
-
 		case 39: // right
 		case 68: // d
-		$('#players').animate({left: "+="+speed}, 10);
+		$('#players').animate({left: "-="+speed}, 10);
+		$('#player_'+window.localplayer).animate({left: "+="+speed}, 10);
 		break;
 
 		case 40: // down
 		case 83: // down
-		$('#players').animate({top:"+="+speed}, 10);
+		$('#players').animate({top:"-="+speed}, 10);
+		$('#player_'+window.localplayer).animate({top:"+="+speed}, 10);
 		break;
 
+		case 37: // left
+		case 65: // a
+		$('#players').animate({left: "+="+speed}, 10);
+		$('#player_'+window.localplayer).animate({left: "-="+speed}, 10);
+		break;
+
+		case 38: // up
+		case 87: // w
+		$('#players').animate({top:"+="+speed}, 10);
+		$('#player_'+window.localplayer).animate({top:"-="+speed}, 10);
+		break;
 		default: return; // exit this handler for other keys
 	}
+	
+	if($('#player_'+window.localplayer).position().left > 10000 || $('#player_'+window.localplayer).position().left < 0 || $('#player_'+window.localplayer).position().top > 10000 || $('#player_'+window.localplayer).position().top < 0){
+		x = 5000 - ($(document).width() / 2);
+		y = 5000 - ($(document).height() / 2);
+		$('#players').css("left", "-"+x);
+		$('#players').css("top", "-"+y);
+		
+		$('#player_'+window.localplayer).css("left", "5000");
+		$('#player_'+window.localplayer).css("top", "5000");
+		alert("Out of bounds!");
+	}
+	
 	e.preventDefault(); // prevent the default action (scroll / move caret)
 });
 
@@ -56,5 +80,3 @@ function player_set_name(){
 		return $.cookie("player");
 	}
 }
-function player_location_x(x){x = ($(document).width() / 2 ) + x;return x;}
-function player_location_y(y){y = ($(document).height() / 2 ) + y;return y;}
