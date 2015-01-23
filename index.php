290aeafb444b167f9a08bfb484df50a01255fc42
Zachary Claret-Scott <github@hcaz.me.uk>
2015-01-23 19:35:54 +0000

    Added loading screen
    Going to display this as a catch for javascript, and while i load more
    scripts in the background and connect with the server

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <html>
 	<head>
 		<title>Untitled</title>
+		<link rel="stylesheet" type="text/css" href="/styles/load.css"/>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<script type="text/javascript" src="/javascript/load.js" defer></script>
 	</head>
 	
 	<body>
-	
+		<div id="loader"><div></div><h1>Loading...</h1><p>Downloading scripts<br />Please make sure JavaScript is enabled.</p></div>
 	</body>
 </html>
