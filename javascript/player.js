ab033967f7a7cbe8328ae79da242ec6e44441cd5
Zachary Claret-Scott <github@hcaz.me.uk>
2015-01-24 18:58:40 +0000

    Updated

--- javascript/player.js
+++ javascript/player.js
@@ -153,14 +153,12 @@ var overlaps = (function () {
         height = $( elem ).height();
         return [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
     }
-
     function comparePositions( p1, p2 ) {
         var r1, r2;
         r1 = p1[0] < p2[0] ? p1 : p2;
         r2 = p1[0] < p2[0] ? p2 : p1;
         return r1[1] > r2[0] || r1[0] === r2[0];
     }
-
     return function ( a, b ) {
         var pos1 = getPositions( a ),
             pos2 = getPositions( b );
