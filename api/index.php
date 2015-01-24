9db6a33295bfd9cf740c8228c976dcb637c83fd4
Zachary Claret-Scott <github@hcaz.me.uk>
2015-01-24 00:42:08 +0000

    Updated to only display movements of latest players(five mins)
    Only players are displayed if moved within five minutes, need to remove
    old players on client end.

--- api/index.php
+++ api/index.php
@@ -5,10 +5,10 @@ if($_REQUEST['player']!=""){
 	if($result->num_rows == 0){
 		$result = $conn->query("INSERT INTO `PLAYERS` (`HANDLE`, `COLOR`, `X`, `Y`, `STAMP`) VALUES ('".$_REQUEST['player']."', 'eeeeee', '".$_REQUEST['playerx']."', '".$_REQUEST['playery']."', UNIX_TIMESTAMP(NOW()));");
 	}else{
-		$result = $conn->query("UPDATE `PLAYERS` SET `X` = '".$_REQUEST['playerx']."', `Y` = '".$_REQUEST['playery']."' WHERE `HANDLE` = '".$_REQUEST['player']."';");
+		$result = $conn->query("UPDATE `PLAYERS` SET `X` = '".$_REQUEST['playerx']."', `Y` = '".$_REQUEST['playery']."', `STAMP` = UNIX_TIMESTAMP(NOW()) WHERE `HANDLE` = '".$_REQUEST['player']."';");
 	}
 }
-$result = $conn->query("SELECT `HANDLE`, `COLOR`, `X`, `Y` FROM `PLAYERS`;");// WHERE `STAMP` > '".(time()-300)."'
+$result = $conn->query("SELECT `HANDLE`, `COLOR`, `X`, `Y` FROM `PLAYERS` WHERE `STAMP` > '".(time()-300)."';");
 while($row = $result->fetch_assoc()) {
 	$players[] = array($row["HANDLE"],$row["COLOR"],$row["X"],$row["Y"]);
 }
